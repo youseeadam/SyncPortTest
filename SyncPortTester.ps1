84ce061b8b8b2b1e415989e019bdd027eac17746
Adam Berns <adamberns@outlook.com>
2020-02-14 07:23:09 -0800

    added host
    r.lr-ingest.io22ulqg35c4-dsn.algolia.net

--- SyncPortTester.ps1
+++ SyncPortTester.ps1
@@ -5,7 +5,8 @@ $Ports = @("sync.logitech.com",
 "a3fejkt9utwjk2-ats.iot.us-west-2.amazonaws.com",
 "a3fejkt9utwjk2-ats.iot.us-east-1.amazonaws.com",
 "e937fa0aeeab484884e6be905b6106bb.us-west-2.aws.found.io",
-"cdn.lr-ingest.io")
+"cdn.lr-ingest.io",
+"r.lr-ingest.io22ulqg35c4-dsn.algolia.net")
 
 foreach ($synchost in $ports) {
     write-Output "Checking IPs for $SyncHost"
